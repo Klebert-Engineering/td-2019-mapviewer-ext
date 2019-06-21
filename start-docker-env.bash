c0e4a020fe69476c9f2d0ce1e8519c9ce59f6c81
Serein Pfeiffer <s.pfeiffer@klebert-engineering.de>
2019-06-21 14:39:58 +0200

    Docker Image: Add utility script to fetch and run image.

---
+++ start-docker-env.bash
@@ -0,0 +1,7 @@
+docker pull artifactory.nds-association.org/tooling-dockerreg/td2019-afw-dev:latest 
+docker run -it \
+           -p 5000:5000 \
+           -v $PWD:/mvo-ext-src \
+           -v $PWD/mvo-cfg:/var/opt/nds/mvo \
+           artifactory.nds-association.org/tooling-dockerreg/td2019-afw-dev:latest \
+           bash
